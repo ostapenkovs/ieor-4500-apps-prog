05c25295f1cfd0543293e436680c4caf114c54aa
Vasiliy Ostapenko <ostapenkovs@pm.me>
2023-12-09 14:44:43 -0500

    wtf

--- hw5/util.py
+++ hw5/util.py
@@ -34,19 +34,20 @@ def g(x: np.ndarray, theta: float, pi: float, delta_bar: np.ndarray, delta_cente
 
 def gradient_descent(x_0: np.ndarray, theta: float, pi: float, alpha: float, beta: float, \
                      num_iter: int, batch_size: int, tolerance: float, delta_bar: np.ndarray, delta_centered: np.ndarray) -> tuple:
-    '''Gradient descent function. Using gradient normalization, momentum, clipping, and batches. Vectorized.'''
+    '''Gradient descent function. Using multiprocessed batches, gradient normalization, momentum, clipping. Vectorized.'''
+    # initial parameters to start gradient descent
     converged = False
-    n = delta_centered.shape[0]
     x = y = x_0
-    # evaluating function at initial value
-    # seeing if we can minimize this!
+    # evaluating function at initial value - seeing if we can minimize this!
     f_val = f(x=x_0, theta=theta, pi=pi, delta_bar=delta_bar, delta_centered=delta_centered)
     hist = [f_val]
-
+    
+    # parameter for getting batches and process pool initialization
+    n = delta_centered.shape[0]
     num_workers = mp.cpu_count()
     pool = mp.Pool(num_workers)
-
-    for iter in tqdm(range(num_iter), leave=True, desc='Iterations'):
+    
+    for _ in tqdm(range(num_iter), leave=True, desc='Iterations'):
         # computing gradient IN PARALLEL
         frozen_grad = partial(g, x, theta, pi, delta_bar)
         grad = pool.map(
@@ -69,7 +70,8 @@ def gradient_descent(x_0: np.ndarray, theta: float, pi: float, alpha: float, bet
         f_new = f(x=x, theta=theta, pi=pi, delta_bar=delta_bar, delta_centered=delta_centered)
         hist.append(f_new)
         # checking convergence
-        if f_val - f_new < tolerance:
+        # (another way to check convergence) if f_val - f_new < tolerance:
+        if np.all( np.abs(x) < tolerance ):
             converged = True
             break
         # new function value is the old function value in the next iteration
@@ -85,43 +87,46 @@ def gradient_descent(x_0: np.ndarray, theta: float, pi: float, alpha: float, bet
 
 def main() -> None:
     '''Testing our gradient descent (parallelized + vectorized).'''
-    ### READING IN THE DATA ####
+    ### READING IN THE DATA #################
     data_folder = './data'
     index_name = 'closeRussell1000'
 
-    # stuff that is used as input to the gradient descent function
     data = dict()
     file_endings = ['delta_bar', 'delta_centered']
     for end in file_endings:
         with open(f'{data_folder}/{index_name}_{end}.pkl', 'rb') as f:
             data[end] = pickle.load(f)
+    #########################################
 
-    n, p = data['delta_centered'].shape
-
-    with open(f'{data_folder}/{index_name}_pair_names.pkl', 'rb') as f:
-        pair_names = pickle.load(f)
-    ############################
-
-    ### TESTING OUR GRADIENT DESCENT ###
+    ### TESTING OUR GRADIENT DESCENT ########
     # initial feasible guess
-    x_0 = np.random.uniform(-1, 1, p)
-
+    p = data['delta_centered'].shape[1]
     converged, x, hist = gradient_descent(
-            x_0=x_0,
-            theta=10, pi=2, alpha=1e-3, beta=0.9,
-            num_iter=5, batch_size=32, tolerance=1e-6,
+            x_0=np.random.uniform(-1, 1, p), #np.zeros(p)
+            theta=10, pi=2, alpha=1e-1, beta=0.9,
+            # 128 is the fastest empirically tested batch size
+            num_iter=50, batch_size=128, tolerance=1e-6,
             **data
         )
-    ####################################
+    
+    data = None
+    #########################################
 
     ### INSPECTING / PLOTTING THE RESULTS ###
-    print(converged)
+    print(f'Did we converge? {converged}. Final function value: {hist[-1]}')
 
     fig, ax = plt.subplots(1, 2, figsize=(12, 4))
     sns.lineplot(hist, ax=ax[0])
     sns.histplot(x, ax=ax[1])
     plt.tight_layout()
     plt.show()
+
+    with open(f'{data_folder}/{index_name}_pair_names.pkl', 'rb') as f:
+        pair_names = np.array( pickle.load(f) )
+    
+    cutoff = 0.75
+    mask = np.abs(x) > cutoff
+    print( pair_names[mask] )
     #########################################
 
 if __name__ == '__main__':
